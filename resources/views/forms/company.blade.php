b4311208c0342fc59d1253df90c3835bd91c4b80
Rob Arnold <roba@neilson.com>
2020-07-30 00:42:18 +0100

    custom js added

--- resources/views/forms/company.blade.php
+++ resources/views/forms/company.blade.php
@@ -8,7 +8,7 @@
         <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
         <div class="col-md-6">
-            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autofocus>
+            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $edit }}" required autofocus>
 
             @error('email')
             <span class="invalid-feedback" role="alert">
@@ -23,7 +23,7 @@
         <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
         <div class="col-md-6">
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autofocus>
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="" autofocus>
 
             @error('email')
             <span class="invalid-feedback" role="alert">
