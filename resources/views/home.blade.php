565d89e18842b8ca5fb928caa6488c763e7d772c
Rob Arnold <roba@neilson.com>
2020-07-29 09:07:55 +0100

    homepage commented out code removed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -172,37 +172,24 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
                     <li class="nav-item has-treeview menu-open">
-                        {{--<a href="#" class="nav-link active">--}}
-                            {{--<i class="nav-icon fas fa-tachometer-alt"></i>--}}
-                            {{--<p>--}}
-                                {{--Starter Pages--}}
-                                {{--<i class="right fas fa-angle-left"></i>--}}
-                            {{--</p>--}}
-                        {{--</a>--}}
+
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
                                 <a href="#" class="nav-link active">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Active Page</p>
+                                    <p>Companies</p>
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Inactive Page</p>
+                                    <p>Employees</p>
                                 </a>
                             </li>
                         </ul>
+
                     </li>
-                    {{--<li class="nav-item">--}}
-                        {{--<a href="#" class="nav-link">--}}
-                            {{--<i class="nav-icon fas fa-th"></i>--}}
-                            {{--<p>--}}
-                                {{--Simple Link--}}
-                                {{--<span class="right badge badge-danger">New</span>--}}
-                            {{--</p>--}}
-                        {{--</a>--}}
-                    {{--</li>--}}
+
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
@@ -235,7 +222,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <div class="container-fluid">
 
                 {{--table here--}}
-                
+
+
 
                 <!-- /.row -->
             </div><!-- /.container-fluid -->
