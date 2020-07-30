476dc5a9a8723a3e45ed72919a8c7ef242d28762
Rob Arnold <roba@neilson.com>
2020-07-30 10:01:08 +0100

    employees with id and delete cascade

--- database/migrations/2020_07_29_081323_create_employees_table.php
+++ database/migrations/2020_07_29_081323_create_employees_table.php
@@ -14,10 +14,14 @@ class CreateEmployeesTable extends Migration
     public function up()
     {
         Schema::create('employees', function (Blueprint $table) {
+            $table->id();
             $table->string('first_name'); //required
             $table->string('last_name'); //required
             $table->string('company'); //required
-            $table->foreign('company')->references('name')->on('companies');
+            $table->foreign('company')
+              ->references('name')
+              ->on('companies')
+              ->onDelete('cascade');
             $table->string('email')->nullable();
             $table->integer('phone')->nullable();
             $table->timestamps();
