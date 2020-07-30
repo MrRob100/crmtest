3a0d4352867f72110502a1a1bdd809f66d72a0ec
Rob Arnold <roba@neilson.com>
2020-07-30 21:59:39 +0100

    employee toggle edit form done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -269,7 +269,94 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                         @foreach ($employees as $key => $employee)
 
-                            @include('forms.employee')
+                            <div style="display:none" class="edit-item" id="edit-item-{{ $employee->id }}">
+
+                            <form id="employee-form" method="POST" action="{{ route('employees.store') }}" enctype="multipart/form-data">
+                                @csrf
+
+                                first_name
+                                <div class="form-group row">
+                                    <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('First name') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ $employee->first_name }}" required autofocus>
+
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                last_name
+                                <div class="form-group row">
+                                    <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last name') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ $employee->last_name }}" required autofocus>
+
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                website
+                                <div class="form-group row">
+                                    <label for="Company" class="col-md-4 col-form-label text-md-right">{{ __('Company') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="company" type="text" class="form-control @error('company') is-invalid @enderror" name="company"  value="{{ $employee->company }}" autofocus>
+
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                email
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $employee->email }}" required autofocus>
+
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                phone
+                                <div class="form-group row">
+                                    <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ $employee->phone }}" required autofocus>
+
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button onclick="hideEdit('{{ $employee->id }}')" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+
+                            </form>
+
+                            </div>
 
                             <tr>
                                 <td id="emp-first-name-{{ $key }}">{{ $employee->first_name }}</td>
@@ -278,7 +365,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                 <td id="emp-email={{ $key }}">{{ $employee->email }}"</td>
                                 <td id="emp-phone={{ $key }}">{{ $employee->phone }}"</td>
                                 <td>
-                                    <button data-toggle="modal" data-target="#createEmpModal" title="edit employee" class="btn btn-default waves-effect"><i class="fa fa-pencil-alt"></i></button>
+                                    <button onclick="showEdit('{{ $employee->id }}')" class="btn btn-default waves-effect"><i class="fa fa-pencil-alt"></i></button>
                                     <button title="delete employee" class="btn btn-outline-danger waves-effect"><i class="fa fa-trash-alt"></i></button>
                                 </td>
                             </tr>
